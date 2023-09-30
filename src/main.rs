4569ab6c2d9ff92ce696d329e8ace51d0d476c07
Sujoy De <sujoy.de345@gmail.com>
2023-09-30 22:18:00 +0530

    mutable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     let x=4;
+    let mut y=5;
     {
         let x=3;
         println!("x is {}",x);
+        println!("y is {}",y);
     }
     println!("x is {}",x);
-    // println!("Hello, world!");
+    println!("y is {}",y);
+    y=6;
+    println!("y is {}",y);
+    println!("Hello, world!");
 }
