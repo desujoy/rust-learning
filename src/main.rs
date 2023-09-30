437ba416f262201214509eee52e72dcbbf5af9b3
Sujoy De <sujoy.de345@gmail.com>
2023-09-30 22:46:03 +0530

    excercise b_functions : done

--- src/main.rs
+++ src/main.rs
@@ -25,16 +25,20 @@ fn main() {
     //
     // If you get stuck, remember that this is *very* similar to what `area_of` does.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
     // 2a. Fix this function to correctly compute the area of a rectangle given
     // dimensions x and y by multiplying x and y and returning the result.
     //
-    return x*y;
+    x*y
     // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
     //            `return` on the last line of a function. Change the last line to be a
     //            "tail expression" that returns a value without using `return`.
     //            Hint: `cargo clippy` will warn you about this exact thing.
+}
+
+fn volume(x: i32,y: i32,z: i32) -> i32{
+    x*y*z
 }
